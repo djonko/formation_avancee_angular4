f4337fa4c3623ddf07b8a02ddcb7b5f875c44426
bobdi <prevostdjonko@gmail.com>
2018-08-11 16:43:54 -0400

    - Ajouter des routes Secondaires

--- fr_431_02_01/childRoutes/src/app/app.module.ts
+++ fr_431_02_01/childRoutes/src/app/app.module.ts
@@ -6,9 +6,12 @@ import { AppComponent } from './app.component';
 import {RouterModule, Routes } from '@angular/router';
 import { CustomersMainComponent } from './customers/customers-main/customers-main.component';
 import { CustomersModule } from './customers/customers.module';
+import { UsersComponent } from './admin/users/users.component';
+import { AdminModule } from './admin/admin.module';
 
 const appRoutes: Routes =[
-  {path:"",redirectTo: "/customers", pathMatch:'full'}
+  {path:"",redirectTo: "/customers", pathMatch:'full'},
+  {path:"admin",component: UsersComponent, outlet:"adminOutlet"}
 ]
 
 @NgModule({
@@ -18,6 +21,7 @@ const appRoutes: Routes =[
   imports: [
     BrowserModule,
     CustomersModule,
+    AdminModule,
     RouterModule.forRoot(appRoutes)
     
   ],
